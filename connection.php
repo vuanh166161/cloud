5a63e9a6df58a8c02c84d739c714a67c7c3c8c01
vuanh166161 <anhnvgcc200163@fpt.edu.vn>
2023-04-05 13:10:24 +0700

    ok

--- connection.php
+++ connection.php
@@ -1,6 +1,5 @@
 <?php
-$conn = pg_connect("postgres://nzkgaewlkswfqs:f39c9543caf44f2fa356c64e47f9c616bee11f3f5fcb5f96207cf4a790a52bd8@ec2-54-208-11-146.compute-1.amazonaws.com:5432/d3esqa1er0rpj9
-");
+$conn = pg_connect("postgres://nzkgaewlkswfqs:f39c9543caf44f2fa356c64e47f9c616bee11f3f5fcb5f96207cf4a790a52bd8@ec2-54-208-11-146.compute-1.amazonaws.com:5432/d3esqa1er0rpj9");
 
 	if(!$conn)
 	{
