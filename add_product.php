05dc0fb4bb0716b004bbe7f83812fb06b7674d71
vuanh166161 <anhnvgcc200163@fpt.edu.vn>
2023-04-09 21:13:43 +0700

    ok 1

--- add_product.php
+++ add_product.php
@@ -129,7 +129,7 @@
 		else{
 			echo $pic['type'];
 			echo $pic['size'];
-			if($pic['type']=="image/jpg"||$pic['type']=="image/jpeg"||$pic['type']=="image/png" ||$pic['type']=="image/gif"){
+			// if($pic['type']=="image/jpg"||$pic['type']=="image/jpeg"||$pic['type']=="image/png" ||$pic['type']=="image/gif"){
 				if($pic['size']<=614400){
 					$sq="SELECT * from product where product_id='$id'or product_name='$proname'";
                     $result= pg_query($conn,$sq);
@@ -155,11 +155,11 @@
 						echo"<li>Duplicate product ID or Name</li>";
 					}
 				}
-            }
-            else
-            {
-                echo "Image fail";
-            }
+            // }
+            // else
+            // {
+            //     echo "Image fail";
+            // }
 		}
 	}
 ?>
