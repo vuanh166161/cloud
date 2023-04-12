2b37116744276210ff679e677bef7e9ba2a2c987
vuanh166161 <anhnvgcc200163@fpt.edu.vn>
2023-04-12 18:55:38 +0700

    finish

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
-$conn = pg_connect("postgres://clcsaimmecpvec:7c0fa48751242b924b628c8eb2a3306f1b59eb6c7e7a7901b5fa656c6836a686@ec2-52-205-45-222.compute-1.amazonaws.com:5432/dfecog678lhqu4");
+$conn = pg_connect("postgres://ndffhofcqjdyju:437a3e7d96e23e7c3399f6d1d2c633fe3c562da30d86a68e77e2d5c04617aa02@ec2-44-215-1-253.compute-1.amazonaws.com:5432/d1206sga70vhhb");
 
 	if(!$conn)
 	{
